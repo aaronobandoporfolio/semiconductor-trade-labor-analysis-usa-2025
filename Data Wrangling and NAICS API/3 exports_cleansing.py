3a30ceb9c0e1904def23af19040badaca3001ae5
Aaron Obando <aaronobandog320@gmail.com>
2026-02-06 20:09:26 -0600

    Enhance export data cleaning process
    Added additional columns to drop and replaced 'X' with 'United States' in the reporterDesc column.

--- Data Wrangling and NAICS API/3 exports_cleansing.py
+++ Data Wrangling and NAICS API/3 exports_cleansing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # Read the CSV file containing USA export trade data
-df = pd.read_csv('C:\\Users\\Aaron\\OneDrive\\Documents\\Debut projects\\HS 3542 Un Comtrade USA Exports.csv')
+df = pd.read_csv("C:\\Users\\Aaron\\OneDrive\\Documents\\Debut projects\\Done\\HS 3542 Un Comtrade USA Exports.csv", encoding='Latin1')
 
 # Convert selected columns to numeric values (if conversion fails, set as NaN)
 for col in ['partnerCode', 'cmdCode', 'qty', 'qtyUnitCode', 'altQtyUnitCode', 'altQty', 'fobvalue', 'primaryValue']:
@@ -12,11 +12,18 @@ for col in ['partnerCode', 'cmdCode', 'qty', 'qtyUnitCode', 'altQtyUnitCode', 'a
 cols_to_drop = [
     'flowCode', 'partner2Code', 'partner2ISO', 'mosCode', 'motCode',
     'netWgt', 'grossWgt', 'legacyEstimationFlag', 'cifvalue',
-    'primaryValue', 'altQtyUnitCode', 'altQtyUnitAbbr', 'altQty'
+    'primaryValue', 'altQtyUnitCode', 'altQtyUnitAbbr', 'altQty', 
+    'isAltQtyEstimated', 'motDesc', 'customsDesc', 'isGrossWgtEstimated', 
+    'isNetWgtEstimated', 'qty', 'qtyUnitCode', 'isAggregate', 
+    'cmdCode', 'partnerCode', 'partnerDesc', 'reporterISO'
 ]
+
 
 # Drop the unnecessary columns from the DataFrame
 df = df.drop(columns=cols_to_drop)
+
+# Replace X with United States
+df['reporterDesc'] = df['reporterDesc'].replace('X', 'United States')
 
 # Save the cleaned DataFrame to a new CSV file
 df.to_csv('Cleaned_HS_3542_Un_Comtrade_USA_Exports.csv', index=False)
