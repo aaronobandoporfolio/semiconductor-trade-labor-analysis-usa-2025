9efce9acea013d21739a29ef00af4d3f3191056d
Aaron Obando <aaronobandog320@gmail.com>
2026-02-09 19:44:49 -0600

    Update 3 exports_cleansing.py

--- Data Wrangling and NAICS API/3 exports_cleansing.py
+++ Data Wrangling and NAICS API/3 exports_cleansing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # Read the CSV file containing USA export trade data
-df = pd.read_csv("C:\\Users\\Aaron\\OneDrive\\Documents\\Debut projects\\Done\\HS 3542 Un Comtrade USA Exports.csv", encoding='Latin1')
+df = pd.read_csv("C:\\Users\\Aaron\\OneDrive\\Documents\\Debut projects\\1 Done\\HS 3542 Un Comtrade USA Exports.csv", encoding='Latin1')
 
 # Convert selected columns to numeric values (if conversion fails, set as NaN)
 for col in ['partnerCode', 'cmdCode', 'qty', 'qtyUnitCode', 'altQtyUnitCode', 'altQty', 'fobvalue', 'primaryValue']:
@@ -11,19 +11,22 @@ for col in ['partnerCode', 'cmdCode', 'qty', 'qtyUnitCode', 'altQtyUnitCode', 'a
 # Define columns that are not needed and should be removed
 cols_to_drop = [
     'flowCode', 'partner2Code', 'partner2ISO', 'mosCode', 'motCode',
-    'netWgt', 'grossWgt', 'legacyEstimationFlag', 'cifvalue',
+    'netWgt','period','grossWgt', 'legacyEstimationFlag', 'cifvalue',
     'primaryValue', 'altQtyUnitCode', 'altQtyUnitAbbr', 'altQty', 
     'isAltQtyEstimated', 'motDesc', 'customsDesc', 'isGrossWgtEstimated', 
     'isNetWgtEstimated', 'qty', 'qtyUnitCode', 'isAggregate', 
-    'cmdCode', 'partnerCode', 'partnerDesc', 'reporterISO'
+    'cmdCode', 'partnerCode', 'reporterISO', 'isReported',
+    'partner2Desc', 'isQtyEstimated'
 ]
 
 
 # Drop the unnecessary columns from the DataFrame
 df = df.drop(columns=cols_to_drop)
 
-# Replace X with United States
-df['reporterDesc'] = df['reporterDesc'].replace('X', 'United States')
+df = df.loc[df['partnerISO'] != 'World']
+
+df['reporterDesc'] = df['reporterDesc'].replace('USA', 'United States')
 
 # Save the cleaned DataFrame to a new CSV file
 df.to_csv('Cleaned_HS_3542_Un_Comtrade_USA_Exports.csv', index=False)
+
